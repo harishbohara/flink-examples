7b50140cee80bfffe1e0842d0fbef43b4a66bbbd
Harish Bohara <harish.bohara@gmail.com>
2022-01-17 11:53:25 +0530

    [skip ci] Added code to transfrom from one kafka to other

--- src/main/java/io/github/devlibx/flink/example/logevent/Pipeline.java
+++ src/main/java/io/github/devlibx/flink/example/logevent/Pipeline.java
@@ -3,10 +3,12 @@ package io.github.devlibx.flink.example.logevent;
 import io.gitbub.devlibx.easy.helper.common.LogEvent;
 import io.gitbub.devlibx.easy.helper.map.StringObjectMap;
 import io.github.devlibx.flink.example.pojo.FlattenLogEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+@Slf4j
 public class Pipeline {
 
     /**
@@ -14,8 +16,11 @@ public class Pipeline {
      */
     SingleOutputStreamOperator<FlattenLogEvent> process(StreamExecutionEnvironment env, DataStream<LogEvent> events, StringObjectMap parameter) {
 
+        // Ensure that we do not send bad events
+        DataStream<LogEvent> filteredEvent = events.filter(le -> le.getEntity() != null);
+
         // Collect items for last 60(n) sec, and emit this data every 5(n) sec
-        return events
+        return filteredEvent
                 // Order by status - you can ignore it and use WindowAll function if required
                 .keyBy(logEvent -> logEvent.getEntity().getType() + "-" + logEvent.getEntity().getId())
                 // Setup event processor
